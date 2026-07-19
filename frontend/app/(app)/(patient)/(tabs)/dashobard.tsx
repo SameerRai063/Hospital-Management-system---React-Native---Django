59b8577c2d074e91977ddf4bb5880ebc331b49f7
SameerRai063 <samirraye7@gmail.com>
2026-07-20 00:40:02 +0545

    halka changes

---
+++ frontend/app/(app)/(patient)/(tabs)/dashobard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const dashobard = () => {
+  return (
+    <View>
+      <Text>dashobard</Text>
+    </View>
+  )
+}
+
+export default dashobard
