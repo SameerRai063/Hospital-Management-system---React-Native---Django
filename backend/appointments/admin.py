f28db3e61fd5a8824b6bc2ac1995699affe84dac
SameerRai063 <samirraye7@gmail.com>
2026-07-22 13:51:16 +0545

    tei model ko attributes haru

--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from backend.appointments.models import Appointment
+
 # Register your models here.
+admin.site.register(Appointment)
--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from backend.users.models import User
+
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return f"Message {self.id} - From: {self.sender} | To: {self.receiver}"
