937b8a5a728652364ce76f7cc1de138597d880ea
SameerRai063 <samirraye7@gmail.com>
2026-07-24 22:32:34 +0545

    postgre sql database connections

--- backend/Hospital/settings.py
+++ backend/Hospital/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'Hospital.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hospital_app",
+        "USER": "postgres",
+        "PASSWORD": "samirrai12345",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- backend/appointments/admin.py
+++ backend/appointments/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from backend.appointments.models import Appointment
+from appointments.models import Appointment
 
 # Register your models here.
 admin.site.register(Appointment)
