f5bab501139248989295af3dd4a4837324d5a4d9
SameerRai063 <samirraye7@gmail.com>
2026-07-25 22:35:03 +0545

    crud operations for patient

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import PatientListView
+from .views import PatientDetailView, PatientListView
 
 urlpatterns = [
     path("patients/", PatientListView.as_view(), name="patient-list"),
+     path(
+        "patients/<str:user_id>/",
+        PatientDetailView.as_view(),
+        name="patient-detail",
+    ),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from backend.users.permissions import IsAdmin
 from backend.users.serializer import PatientRegisterSerializer
@@ -19,7 +19,7 @@ class PatientListView(ListAPIView):
 
 class PatientDetailView(RetrieveAPIView):
     queryset = Patient.objects.select_related("user")
-    serializer_class = PatientDetailSerializer
+    serializer_class = PatientListSerializer
     permission_classes = [IsAuthenticated, IsAdmin]
 
     lookup_field = "user__user_id"
