2d4f803fe4c8585ef328541abf57d20516352afa
SameerRai063 <samirraye7@gmail.com>
2026-07-26 20:15:39 +0545

    urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import PatientDetailView, PatientListView
+from .views import DoctorCreateAPIView, PatientDetailView, PatientListView, PatientRegisterView
 
 urlpatterns = [
+    #Patients URLS-------------------------------------------------------------------------------------------------------
     path("patients/", PatientListView.as_view(), name="patient-list"),
      path(
         "patients/<str:user_id>/",
         PatientDetailView.as_view(),
         name="patient-detail",
     ),
+    path("register/", PatientRegisterView.as_view(), name="patient-register"),
+    #Doctors URLS-------------------------------------------------------------------------------------------------------
+    path("doctors/", DoctorCreateAPIView.as_view(), name="doctor-create"),
     
 ]
